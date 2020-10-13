cf1ecaf5909cc353ca3cb64a07ca90a44a35b72e
Satwik Ram <satwikram29@gmail.com>
2020-10-13 13:39:52 +0530

    Added Requirements

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def search(request):
+    return render(request, 'search.html')
