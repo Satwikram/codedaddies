dbe248bff839d0e0053e4a3e02a120f191ed9f41
Satwik Ram <satwikram29@gmail.com>
2020-10-13 15:30:29 +0530

    Added BS4

--- my_app/views.py
+++ my_app/views.py
@@ -1,10 +1,33 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
+from django.urls import reverse
+from requests.compat import quote_plus
+from .models import Search
+
+BASE_CRAIGLIST_URL = 'https://bangalore.craigslist.org/search/bbb?query={}'
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
 def search(request):
-    return render(request, 'search.html')
+    if request.method == "POST":
+        search = request.POST.get('search')
+        final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
+        sobj = Search.objects.create(search = search)
+        sobj.save()
+        print("Final URL:", final_url)
+        response = requests.get(final_url)
+        data = response.text
+        soup = BeautifulSoup(data, features = 'html.parser')
+        post_titles = soup.find_all('a', {'class' : 'result-title'})
+        print(post_titles)
+
+        context = {
+            'data': data,
+        }
+        return HttpResponseRedirect(reverse('search'))
+    else:
+        return render(request, 'base.html')
