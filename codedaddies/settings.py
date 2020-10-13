906b39cd63f08e883cc582a21df832d25a5b1f3d
Satwik Ram <satwikram29@gmail.com>
2020-10-13 17:35:54 +0530

    Settings updated

--- codedaddies/settings.py
+++ codedaddies/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from secret import *
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,9 +77,9 @@ WSGI_APPLICATION = 'codedaddies.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': dbname,
+        'NAME': 'codedaddies',
         'HOST': 'localhost',
-        'PASSWORD': password,
+        'PASSWORD': 'pass6671',
         'USER': 'postgres'
     }
 }
